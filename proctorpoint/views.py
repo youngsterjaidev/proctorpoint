2f1cb0462efd96827ebd3d299b67f776aa4add28
Avilipsa12345 <157614469+Avilipsa12345@users.noreply.github.com>
2025-06-25 15:16:21 +0530

    hii

--- proctorpoint/views.py
+++ proctorpoint/views.py
@@ -1,5 +1,20 @@
-from django.http import HttpResponse
+tempQuiz = {
+    "title": "Python Quiz",
+    "timeLimit": 300,
+    "question": [
+        {
+            "questionText": "What function is used to print output to the console?",
+            "options": ["console.log()", "print()", "echo()", "output()"],
+            "correctAnswerIndex": 1
+        }
+    ]
+}
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hi Dev!")
+
+
+# Create that send the quiz data to client
+def get_all_quiz(request):
+    return JsonResponse(tempQuiz)
